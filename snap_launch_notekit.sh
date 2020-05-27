f1604e2aa7a6b947100f21fa6582ebfcabdc3e0f
noasakurajin <noasakurajin@web.de>
2020-05-27 14:40:47 +0200

    improved launch script

--- snap_launch_notekit.sh
+++ snap_launch_notekit.sh
@@ -6,6 +6,8 @@ cd /snap/notekit/current/usr/share/notekit
 
 if [ ! -f $SNAP_USER_DATA/.config/notekit/config.json ]
 then
+    mkdir -p $SNAP_USER_DATA/.config
+    mkdir -p $SNAP_USER_DATA/.config/notekit
     cp data/default_config.json $SNAP_USER_DATA/.config/notekit/config.json
 fi
 
