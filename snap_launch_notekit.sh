7a65e6c9fe84ef5ce70bf368d864dda4ea7c453e
noasakurajin <noasakurajin@web.de>
2020-05-27 13:22:53 +0200

    copying default config before launch if needed

--- snap_launch_notekit.sh
+++ snap_launch_notekit.sh
@@ -3,6 +3,12 @@
 set -x
 
 cd /snap/notekit/current/usr/share/notekit
+
+if [ ! -f $SNAP_USER_DATA/.config/notekit/config.json ]
+then
+    cp data/default_config.json $SNAP_USER_DATA/.config/notekit/config.json
+fi
+
 ./notekit
 
 set +x
