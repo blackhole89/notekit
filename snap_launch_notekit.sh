d60d96ed015a76456f9545f75345fbb3b0d1e83c
noasakurajin <noasakurajin@web.de>
2020-05-29 11:28:22 +0200

    using clatexmath build and SNAP_USER_COMMON for data

--- snap_launch_notekit.sh
+++ snap_launch_notekit.sh
@@ -5,14 +5,14 @@ set -x
 cd "$SNAP/usr/share/notekit"
 
 export XDG_DATA_DIRS="$SNAP/usr/share:$XDG_DATA_DIRS"
-export XDG_CONFIG_HOME="$SNAP_USER_DATA"
-export XDG_DATA_HOME="$SNAP_USER_DATA"
+export XDG_CONFIG_HOME="$SNAP_USER_COMMON"
+export XDG_DATA_HOME="$SNAP_USER_COMMON"
 
-if [ ! -f $SNAP_USER_DATA/.config/notekit/config.json ]
+if [ ! -f $SNAP_USER_COMMON/.config/notekit/config.json ]
 then
-    mkdir -p $SNAP_USER_DATA/.config
-    mkdir -p $SNAP_USER_DATA/.config/notekit
-    cp data/default_config.json $SNAP_USER_DATA/.config/notekit/config.json
+    mkdir -p $SNAP_USER_COMMON/.config
+    mkdir -p $SNAP_USER_COMMON/.config/notekit
+    cp data/default_config.json $SNAP_USER_COMMON/.config/notekit/config.json
 fi
 
 ./notekit
