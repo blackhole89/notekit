94076dbb84f0607d79e960e629af3398813ac18a
Matvey Soloviev <blackhole89@gmail.com>
2021-05-13 09:53:39 -0400

    Pin cLaTeXMath to a known working version.

--- install-clatexmath.sh
+++ install-clatexmath.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git clone https://github.com/NanoMichael/cLaTeXMath.git
 cd cLaTeXMath
-git reset --hard HEAD
+git reset --hard 7cbc0b9e4185da3a7f633473a794ee1ef7b371e8
 echo 'add_library(clatexmath STATIC ${SRC})' >> CMakeLists.txt
 cmake -DCMAKE_BUILD_TYPE=Release -DHAVE_LOG=OFF -DGRAPHICS_DEBUG=OFF .
 make
