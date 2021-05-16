d488e2a3b7af36b4268852aeaae3861e504194bf
大黄老鼠 <2931408816@qq.com>
2021-05-16 08:30:34 +0800

    fix: break change for cLaTeXMath library (#95)
    see
    https://github.com/NanoMichael/cLaTeXMath/commit/0359f830d381e400d9941436c63c2ecfe8c197a7#commitcomment-50769555
    https://github.com/NanoMichael/cLaTeXMath/pull/68
    https://github.com/NanoMichael/cLaTeXMath/pull/70

--- install-clatexmath.sh
+++ install-clatexmath.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 git clone https://github.com/NanoMichael/cLaTeXMath.git
 cd cLaTeXMath
-git reset --hard 7cbc0b9e4185da3a7f633473a794ee1ef7b371e8
-echo 'add_library(clatexmath STATIC ${SRC})' >> CMakeLists.txt
+git reset --hard HEAD
 cmake -DCMAKE_BUILD_TYPE=Release -DHAVE_LOG=OFF -DGRAPHICS_DEBUG=OFF .
 make
 cp -r res ../data/latex
- 
